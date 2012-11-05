32249e9777a8a13de90cbbd4872dfbd889efd410
Xiang LIu <liuxiang625@gmail.com>
2012-11-05 01:29:51 -0800

    lsof capture method refactored, going to refactore class

--- src/traceCollectors/ProcessbuilderForExec.java
+++ src/traceCollectors/ProcessbuilderForExec.java
@@ -4,55 +4,72 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.regex.Matcher;
 import com.jezhumble.javasysmon.JavaSysMon;
-import com.jezhumble.javasysmon.ProcessInfo;
 
 public class ProcessbuilderForExec {
 	public static void main(String[] args) throws Exception
 	{
+////		JavaSysMon monitor =   new JavaSysMon();
+////	    String osName =        monitor.osName();
+////	    String workingDirectory = new File("").getAbsolutePath();
+////	    int currentPID = monitor.currentPid() ;
+////	    		
+////	    System.out.println("OS name: " + osName + ". PID: " + currentPID + "Working directory: " + workingDirectory);
+////		List<String> command = new ArrayList<String>();
+//		//command.add("lsof");
+//		//command.add("-i");
+//		//command.add("TCP");
+//		//command.add("-p");
+//		//command.add("512");
+////		command.add("/bin/sh");
+////		command.add("-c");
+////		command.add("lsof -p "+ currentPID +" |grep TCP");
+////
+////		ProcessBuilder pb = new ProcessBuilder("/bin/bash",workingDirectory+ "/src/scriptParsers/testScript/lsofScript.sh",""+ 323);///home/xiang/workspaceEE/SaasWatch_SJSU/src/scriptParsers/testScript/lsofScript.sh
+////		Process p = pb.start();
+////		String output = loadStream(p.getInputStream());
+////		String error  = loadStream(p.getErrorStream());
+////		int rc = p.waitFor();
+////		System.out.println("Process ended with rc=" + rc);
+			SaasConnectivityTrace capturedTrace = connectionInfoCollector(1,"Test",new Date());
+			System.out.println(capturedTrace.SaasFd);
+	}
+
+	public static SaasConnectivityTrace connectionInfoCollector(int agentID, String userTag, Date requestTime) throws Exception
+	{
+		//List<SaasConnectivityTrace> tracesArray = new ArrayList<SaasConnectivityTrace>();
 		JavaSysMon monitor =   new JavaSysMon();
 	    String osName =        monitor.osName();
 	    String workingDirectory = new File("").getAbsolutePath();
 	    int currentPID = monitor.currentPid() ;
-	    		
 	    System.out.println("OS name: " + osName + ". PID: " + currentPID + "Working directory: " + workingDirectory);
 		List<String> command = new ArrayList<String>();
-		//command.add("lsof");
-		//command.add("-i");
-		//command.add("TCP");
-		//command.add("-p");
-		//command.add("512");
 		command.add("/bin/sh");
 		command.add("-c");
 		command.add("lsof -p "+ currentPID +" |grep TCP");
-
 		ProcessBuilder pb = new ProcessBuilder("/bin/bash",workingDirectory+ "/src/scriptParsers/testScript/lsofScript.sh",""+ 323);///home/xiang/workspaceEE/SaasWatch_SJSU/src/scriptParsers/testScript/lsofScript.sh
 		Process p = pb.start();
-		String output = loadStream(p.getInputStream());
-		String error  = loadStream(p.getErrorStream());
+		//String output = loadStream(p.getInputStream());
+		//String error  = loadStream(p.getErrorStream());
 		int rc = p.waitFor();
 		System.out.println("Process ended with rc=" + rc);
-		System.out.println("\nStandard Output:\n");
-		System.out.println(output);
-		System.out.println("\nStandard Error:\n");
-		System.out.println(error);
-	}
-
-	private static String loadStream(InputStream s) throws Exception
-	{
-		//List<SaasConnectivityTrace> tracesArray = new ArrayList<SaasConnectivityTrace>();
-		BufferedReader br = new BufferedReader(new InputStreamReader(s));
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
 		StringBuilder sb = new StringBuilder();
 		String line;
-		SaasConnectivityTrace trace = new SaasConnectivityTrace("Test",1,1);
+		SaasConnectivityTrace trace = new SaasConnectivityTrace(userTag,agentID,currentPID);
 		while((line=br.readLine()) != null){
 			Matcher matchedResult = RegexParsers.lsofParser(line);
-			if(trace.traceTime == null) {
+			if(requestTime == null) {
 				trace.traceTime = new Date();
+			}
+			else {
+				trace.traceTime = requestTime;
 			}
 			if(matchedResult.find()) {
 				trace.agentID = 1;
@@ -66,6 +83,9 @@ public class ProcessbuilderForExec {
 				System.out.println("Matched: "+ connection.connectionType);
 			}
 		}
-		return sb.toString();
+		
+		System.out.println("\nStandard Output/Error:\n");
+		System.out.println(sb.toString());
+		return trace;
 	}	
 }
