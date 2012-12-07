4f438fb7bf06bf272c9001c5d6381268739797b4
Xiang Liu <liuxiang625@gmail.com>
2012-12-07 02:03:34 -0800

    GUI Overview replaced wit high Chart

--- SaasWatch_GUI/SaasWatch/SaasWatch/WebFolder/index.waPage/scripts/index.js
+++ SaasWatch_GUI/SaasWatch/SaasWatch/WebFolder/index.waPage/scripts/index.js
@@ -1,11 +1,12 @@
 ﻿
+
+
+
 WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region namespaceDeclaration// @startlock
 	var checkbox2 = {};	// @checkbox
-	var richText15 = {};	// @richText
 	var button2 = {};	// @button
-	var richText3 = {};	// @richText
 	var imageButton3 = {};	// @buttonImage
 	var button5 = {};	// @button
 	var button4 = {};	// @button
@@ -13,14 +14,74 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	var imageButton2 = {};	// @buttonImage
 	var imageButton1 = {};	// @buttonImage
 	var combobox2 = {};	// @combobox
-	var richText1 = {};	// @richText
-	var chart1 = {};	// @chart
 	var setting = {};	// @button
 	var documentEvent = {};	// @document
 	var passwordField = {};	// @textField
 	var button1 = {};	// @button
 // @endregion// @endlock
-
+var chart1; // globally available
+$(document).ready(function() {
+      chart1 = new Highcharts.Chart({
+         chart: {
+            renderTo: 'container1',
+            type: 'line'
+         },
+         title: {
+            text: 'CPU Metrics'
+         },
+         xAxis: {
+            categories: ['0', '1', '2',"3","4","5","6","7","8","9","10","11","12"]
+         },
+         yAxis: {
+            title: {
+               text: 'Percentage'
+            }
+         },
+//         series: [{
+//            name: 'SaasWatch',
+//            data: [1,2,4]
+//         }, {
+//            name: 'Amazon Metrics',
+//            data: [5, 7, 3]
+//         }]
+      });
+      
+      chart2 = new Highcharts.Chart({
+         chart: {
+            renderTo: 'container3',
+            type: 'line'
+         },
+         title: {
+            text: 'Memory Metrics'
+         },
+         xAxis: {
+            categories: ['0', '1', '2',"3","4","5","6","7","8","9","10","11","12"]
+         },
+         yAxis: {
+            title: {
+               text: 'usage'
+            }
+         }
+      });
+      
+      chart3 = new Highcharts.Chart({
+         chart: {
+            renderTo: 'container10',
+            type: 'line'
+         },
+         title: {
+            text: 'Disk Metrics'
+         },
+         xAxis: {
+            categories: ['0', '1', '2',"3","4","5","6","7","8","9","10","11","12"]
+         },
+         yAxis: {
+            title: {
+               text: 'Allocated MegeBytes'
+            }
+         }
+      });
+   });
 // eventHandlers// @lock
 
 	checkbox2.change = function checkbox2_change (event)// @startlock
@@ -28,41 +89,10 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 		// Add your code here
 	};// @lock
 
-	richText15.click = function richText15_click (event)// @startlock
-	{// @endlock
-		$$('tabView2').selectTab(4);
-	};// @lock
-
 	button2.click = function button2_click (event)// @startlock
 	{// @endlock
 		if(WAF.directory.logout());
 		$$('tabView2').selectTab(1);
-	};// @lock
-
-	richText3.click = function richText3_click (event)// @startlock
-	{// @endlock
-		$$('tabView2').selectTab(4);
-		//$$('chart4').recreateChart([ "7" ,  "8" ,  "9","10","11","12","13","14" ] , [ [22, 25, 30,50,10,22,12,0] ] );
-		
-				sources.memoryMetrics.toArray("month, date, hour,minute, usage", {
-       	 		orderBy: "dateOfEntry DESC",
-        		// sorted by first name and by courses in a sub-array
-        		onSuccess: function(event) // asynchronous call
-        		{
-        			$$('richText2').setValue('Memory Monitor');
-        			var labelArray = [];
-        			var valueArray = [];
-            		myArray = event.result; // we retrieve the array of objects
-        			
-        			for (obj in myArray){
-        				if (myArray[obj].month >= 11 & myArray[obj].hour == (new Date()).getHours() + 7){// use to control the data range, will add it later;
-        					labelArray.push(myArray[obj].minute);
-        					valueArray.push(myArray[obj].usage);
-        				}
-        			}
-        			//$$('chart4').recreateChart(labelArray , [ valueArray ] );
-        		}
-    		});
 	};// @lock
 
 	imageButton3.click = function imageButton3_click (event)// @startlock
@@ -78,6 +108,12 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	button4.click = function button4_click (event)// @startlock
 	{// @endlock
 		$$('tabView2').selectTab(2);
+		
+		sources.cpuMetrics.query('ID',sources.cpuMetrics.ID + sources.cpuMetrics.length - 24, {
+			onSuccess: function(event) {
+				var cpuCollection = event.entityCollection;
+        	}, params: ["Barrett", "James"]
+  	  	});
 	};// @lock
 
 	startHistoryTracking.click = function startHistoryTracking_click (event)// @startlock
@@ -133,37 +169,6 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 		}
 	};// @lock
 
-	richText1.click = function richText1_click (event)// @startlock
-	{// @endlock
-		$$('tabView2').selectTab(4);
-		//$$('chart4').recreateChart([ "7" ,  "8" ,  "9","10","11","12","13","14" ] , [ [22, 25, 30,50,10,22,12,0] ] );
-		
-				sources.cpuMetrics.toArray("month, day, hour, percentage", {
-       	 		orderBy: "hour",
-        		// sorted by first name and by courses in a sub-array
-        		onSuccess: function(event) // asynchronous call
-        		{
-        			var labelArray = [];
-        			var valueArray = [];
-            		myArray = event.result; // we retrieve the array of objects
-        			
-        			for (obj in myArray){
-        				if (myArray[obj].month >= 11){// use to control the data range, will add it later;
-        					labelArray.push(myArray[obj].hour);
-        					valueArray.push(myArray[obj].percentage);
-        				}
-        			}
-        			//$$('chart4').recreateChart(labelArray , [ valueArray ] );
-        		}
-    		});
-		
-	};// @lock
-
-	chart1.click = function chart1_click (event)// @startlock
-	{// @endlock
-		$$('tabView2').selectTab(4);
-	};// @lock
-
 	setting.click = function setting_click (event)// @startlock
 	{// @endlock
 		$$('tabView2').selectTab(3);
@@ -172,8 +177,90 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	documentEvent.onLoad = function documentEvent_onLoad (event)// @startlock
 	{// @endlock
 		if (WAF.directory.currentUser()) {
+
+			//Add data to CPU chart 
+        	var cpuData = [];
+        	var amazonCPUData = [];
+        	var memData = [];
+        	var amazonMemData = [];
+        	
+        	sources.cPU.toArray ("percentage, isFromAmazon", 
+			{
+			    top: 40, 
+			    orderBy: "ID desc", 
+			    onSuccess: function(event)  
+			    { 
+			        var cpuArray = event.result;
+			        for (i in cpuArray){
+			        	if (cpuArray[i].isFromAmazon != true & cpuData.length < 12)
+			        	cpuData.push(cpuArray[i].percentage);
+			        	else if (cpuArray[i].isFromAmazon == true & amazonCPUData.length < 12)
+			        	amazonCPUData.push(cpuArray[i].percentage);
+			        	
+			        }
+			        chart1.addSeries({
+				 		name: 'SaasWatch',
+                		data: cpuData        
+           			 });
+           			 chart1.addSeries({
+				 		name: 'Amazon CloudWatch',
+                		data: amazonCPUData        
+           			 });
+           			 
+			    }
+			});
+			
+			sources.memory.toArray ("usage, isFromAmazon", 
+			{
+			    top: 40, 
+			    orderBy: "ID desc", 
+			    onSuccess: function(event)  
+			    { 
+			        var memArray = event.result;
+			        for (i in memArray){
+			        	if (memArray[i].isFromAmazon != true & memData.length < 12)
+			        	memData.push(memArray[i].usage);
+			        	else if (memArray[i].isFromAmazon == true & amazonMemData.length < 12)
+			        	amazonMemData.push(memArray[i].usage);
+			        	
+			        }
+			        chart2.addSeries({
+				 		name: 'SaasWatch',
+                		data: memData        
+           			 });
+           			 chart2.addSeries({
+				 		name: 'Amazon CloudWatch',
+                		data: amazonMemData        
+           			 });
+           			 
+			    }
+			});
+			
+			var diskData = [];
+			sources.disk.toArray ("allocated", 
+			{
+			    top: 12, 
+			    orderBy: "ID desc", 
+			    onSuccess: function(event)  
+			    { 
+			        var diskArray = event.result;
+			        for (i in diskArray){
+			        	diskData.push(diskArray[i].allocated);
+			        }
+			        chart3.addSeries({
+				 		name: 'SaasWatch',
+                		data: diskData        
+           			 });
+			    }
+			});
+			
+			
+			
+			
 			$$('tabView2').selectTab(2);
 		}
+		
+		
 		//$$('chart6').recreateChart([ "7" ,  "8" ,  "9","10","11","12","13","14" ] , [ [22, 25, 30,50,10,22,12,0] ] );
 		//$$('chart2').recreateChart([ "7" , "8" ,  "9","10","11","12","13","14" ] , [ [22, 25, 30,50,10,22,12,0] ] );
 	};// @lock
@@ -223,9 +310,7 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 
 // @region eventManager// @startlock
 	WAF.addListener("checkbox2", "change", checkbox2.change, "WAF");
-	WAF.addListener("richText15", "click", richText15.click, "WAF");
 	WAF.addListener("button2", "click", button2.click, "WAF");
-	WAF.addListener("richText3", "click", richText3.click, "WAF");
 	WAF.addListener("imageButton3", "click", imageButton3.click, "WAF");
 	WAF.addListener("button5", "click", button5.click, "WAF");
 	WAF.addListener("button4", "click", button4.click, "WAF");
@@ -233,8 +318,6 @@ WAF.onAfterInit = function onAfterInit() {// @lock
 	WAF.addListener("imageButton2", "click", imageButton2.click, "WAF");
 	WAF.addListener("imageButton1", "click", imageButton1.click, "WAF");
 	WAF.addListener("combobox2", "change", combobox2.change, "WAF");
-	WAF.addListener("richText1", "click", richText1.click, "WAF");
-	WAF.addListener("chart1", "click", chart1.click, "WAF");
 	WAF.addListener("setting", "click", setting.click, "WAF");
 	WAF.addListener("document", "onLoad", documentEvent.onLoad, "WAF");
 	WAF.addListener("passwordField", "keyup", passwordField.keyup, "WAF");
